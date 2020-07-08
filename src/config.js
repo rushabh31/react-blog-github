a62880894193a35e2baa7be9079829a942e73b87
rushabh31 <rushabh.patel5@gmail.com>
2020-07-07 21:42:47 -0400

    pushing updated

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export const config = {
   // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
-  githubConvertedToken: "MzM2N2YxZDRjNDk5MmQ4MGVjNzQwYmM5Y2FlZWYyNDE0ZGQ4NWFiYQ==",
+  githubConvertedToken: "OWQ0NmM4YjA1MmQ3YzdhZWE1NjkzYWFiZTZkZmEwOTZmNGViZTE3Nw==",
 
   // Your Github UserName
-  githubUserName: "saadpasta",
+  githubUserName: "rushabh31",
 
   // Your Github Repo Name Where You Have your issues as Blog
   githubRepo: "react-blog-github",
@@ -11,14 +11,14 @@ export const config = {
   // Set it to true if you have a Github app to add to this project
   // and fill the client ID & secret
   enableOAuth: true,
-  OAuthClientID: 'Iv1.9f40fd53257d4102',
-  OAuthSecret: 'cc3db76ffe9848c92d83172807eececa79f0eb22',
+  OAuthClientID: 'Iv1.2ceac336af8615a3',
+  OAuthSecret: '11e9d73829db30fd85e796115997108e6b453397',
 
   // Your Personal Blog Title
-  title : "Saad Pasta" ,
+  title : "Welcome to Rushabh's Blogsite" ,
 
   // Your Personal Blog Subtitle
-  subtitle : "Software Developer",
+  subtitle : "Data Science/ML Blogs",
 
   // Header customization
   header: {
