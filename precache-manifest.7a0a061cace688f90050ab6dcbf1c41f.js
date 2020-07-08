f58ba6b54e9014df63609745385d9aa85db6ae75
rushabh31 <rushabh.patel5@gmail.com>
2020-07-07 22:18:10 -0400

    Updates

---
+++ precache-manifest.7a0a061cace688f90050ab6dcbf1c41f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38b89067a322cce81960eefb2bb1f56",
+    "url": "/react-blog-github/index.html"
+  },
+  {
+    "revision": "5dd8df691a022e0af38d",
+    "url": "/react-blog-github/static/css/main.abb3e5cc.chunk.css"
+  },
+  {
+    "revision": "ad2e543783312f970a56",
+    "url": "/react-blog-github/static/js/2.c31b41a1.chunk.js"
+  },
+  {
+    "revision": "3a1793f6de26a25edda42ca61e6e2a4d",
+    "url": "/react-blog-github/static/js/2.c31b41a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dd8df691a022e0af38d",
+    "url": "/react-blog-github/static/js/main.263cebc1.chunk.js"
+  },
+  {
+    "revision": "bf3fa5bb138797a965b0",
+    "url": "/react-blog-github/static/js/runtime-main.447f88b1.js"
+  }
+]);
